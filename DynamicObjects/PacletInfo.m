d4d68ee10042d6f9ce146c3d386c37c3b8ca1a22
Kuba Podkalicki <kuba.pod@gmail.com>
2018-06-06 22:35:29 +0200

    0.1.0

--- DynamicObjects/PacletInfo.m
+++ DynamicObjects/PacletInfo.m
@@ -1,5 +1,5 @@
 (* Created with the Wolfram Language : www.wolfram.com *)
-Paclet[Name -> "DynamicObjects", Version -> "0.0.1", 
+Paclet[Name -> "DynamicObjects", Version -> "0.1.0", 
  MathematicaVersion -> "10.4+", Description -> 
   "Mini framework helping to scale Dynamics up", 
  Creator -> "Kuba Podkalicki (kuba.pod@gmail.com)", 
